20f186e1f45de55f97de4ba7fcede4608c79024f
Irisson Rodrigo <irissoncoimbra@gmail.com>
2023-10-03 11:40:07 -0300

    up

---
+++ analise-dados-shoppings/app.py
@@ -0,0 +1,14 @@
+import pandas as pd
+
+#Importar a base de dados
+tabela_vendas = pd.read_excel('vendas.xlsx')
+
+#Visualizar a base de dados
+
+#Faturamento por Loja
+
+#Quantidade de produtos vendidos
+
+#Ticket meio dos produtos por loja
+
+#Enviar um email com o relatório
